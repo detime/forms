7d8bcb47e78658e17937058ad9de2cd567201920
detime001 <35330681+detime@users.noreply.github.com>
2021-06-26 23:57:58 +0500

    update Sender.js var -> let

--- Sender.js
+++ Sender.js
@@ -3,36 +3,36 @@
 */
 
 
-var myLocaltion = location.href;
+let myLocaltion = location.href;
 myLocaltion = myLocaltion.replace('/viewform', '/formResponse');
 console.log(myLocaltion);
 
 /*@@@@@@@@-Promo Code-@@@@@@@@*/
-var scriptSearch = document.getElementsByTagName('script')[4];
-var patPromo = /\[\"\w+\"\]/gi; /*pattern for search promo code from script*/
-var txts = scriptSearch.firstChild;
-var promoCodes = txts.textContent.toString().match(patPromo)[0].replace(/\[|\"|\]/gi, '');
+let scriptSearch = document.getElementsByTagName('script')[4];
+let patPromo = /\[\"\w+\"\]/gi; /*pattern for search promo code from script*/
+let txts = scriptSearch.firstChild;
+let promoCodes = txts.textContent.toString().match(patPromo)[0].replace(/\[|\"|\]/gi, '');
 console.log('Промокод найден: ' + promoCodes);
 /*@@@@@@@@-Promo Code-@@@@@@@@*/
 
 /*@@@@@@@@-Search for input fields-@@@@@@@@*/
-var entryNames = document.getElementsByTagName('input');
-var inputLogins = entryNames[1].name;
-var inputPromoCodes = entryNames[0].name;
+let entryNames = document.getElementsByTagName('input');
+let inputLogins = entryNames[1].name;
+let inputPromoCodes = entryNames[0].name;
 console.log('Поле для ника: ' + inputLogins, 'Поле для промо: ' + inputPromoCodes);
 /*@@@@@@@@-Search for input fields-@@@@@@@@*/
 
-var loginList = [
+let loginList = [
   'MyLogin'
 ];
-var count = 0; /*dispatch count*/
-var lens = loginList.length; /*number of names*/
+let count = 0; /*dispatch count*/
+let lens = loginList.length; /*number of names*/
 
-var xhr1 = new XMLHttpRequest();
-var pattern = /\/\w+.$/gi;
+let xhr1 = new XMLHttpRequest();
+let pattern = /\/\w+.$/gi;
 
 function f() {
-  var data1 = inputPromoCodes + '=' + promoCodes + '&' + inputLogins + '=' + encodeURI(loginList[count]);
+  let data1 = inputPromoCodes + '=' + promoCodes + '&' + inputLogins + '=' + encodeURI(loginList[count]);
 
   console.log(count + '===========================================')
   console.log(decodeURI(data1));
@@ -43,7 +43,7 @@ function f() {
 
 
   xhr1.onload = function() {
-    var myStatus = xhr1.responseURL.match(pattern)[0];
+    let myStatus = xhr1.responseURL.match(pattern)[0];
     if (myStatus == '/formResponse') {
 
       console.log('Отправлено ststus: ' + myStatus);
